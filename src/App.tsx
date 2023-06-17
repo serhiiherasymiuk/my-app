7ee2011c2e1f662b0aa6fd0b73295d6f367fa013
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-06-18 01:29:42 +0300

    Added ability to edit categories

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ interface ICategory {
 
 function App() {
   const [categories, setCategories] = useState<ICategory[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
 
   const categorySchema = Yup.object().shape({
     name: Yup.string()
@@ -21,7 +22,10 @@ function App() {
         const { path, createError } = this;
         const categoryExists = categories.some(
           (category: ICategory) =>
-            category.name.toLowerCase() === value.toLowerCase()
+            category.name.toLowerCase() === value.toLowerCase() &&
+            value !=
+              categories.find((category) => category.id === selectedCategory)
+                ?.name
         );
         return categoryExists
           ? createError({ path, message: "Category already exists" })
@@ -44,6 +48,35 @@ function App() {
     name: "",
     image: "",
     description: "",
+  };
+
+  const [editValues, setEditValues] = useState<ICategory>({
+    id: 0,
+    name: "",
+    image: "",
+    description: "",
+  });
+
+  const handleEdit = async (values: ICategory) => {
+    try {
+      await categorySchema.validate(values);
+
+      if (selectedCategory !== null) values.id = selectedCategory;
+
+      const response = await axios.post(
+        `http://laravel.pv125.com/api/category/edit`,
+        values
+      );
+      const updatedCategory: ICategory = response.data;
+      const updatedCategories = categories.map((category) =>
+        category.id === updatedCategory.id ? updatedCategory : category
+      );
+      setCategories(updatedCategories);
+
+      setSelectedCategory(null);
+    } catch (error) {
+      console.error("Error editing category:", error);
+    }
   };
 
   const handleSubmit = async (values: ICategory) => {
@@ -140,40 +173,137 @@ function App() {
         )}
       </Formik>
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Image</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {categories.map((c) => {
-            return (
-              <tr key={c.id}>
-                <th scope="row">{c.id}</th>
-                <td>{c.name}</td>
-                <td>{c.description}</td>
-                <td>
-                  <img width={50} src={c.image} alt="" />
-                </td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-danger btn-sm"
-                    onClick={() => handleDelete(c.id)}
-                  >
-                    <i className="bi bi-trash3"></i>
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <Formik
+        initialValues={editValues}
+        onSubmit={handleEdit}
+        validationSchema={categorySchema}
+        enableReinitialize={true}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Image</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {categories.map((c) => {
+                  const isCategorySelected = selectedCategory === c.id;
+                  return (
+                    <React.Fragment key={c.id}>
+                      {!isCategorySelected && (
+                        <tr
+                          onClick={() => {
+                            setSelectedCategory(c.id);
+                            setEditValues(c);
+                          }}
+                        >
+                          <th scope="row">{c.id}</th>
+                          <td>{c.name}</td>
+                          <td>{c.description}</td>
+                          <td>
+                            <img width={50} src={c.image} alt="" />
+                          </td>
+                          <td>
+                            <button
+                              type="button"
+                              className="btn btn-danger btn-sm"
+                              onClick={() => handleDelete(c.id)}
+                            >
+                              <i className="bi bi-trash3"></i>
+                            </button>
+                          </td>
+                        </tr>
+                      )}
+                      {isCategorySelected && (
+                        <tr>
+                          <th scope="row">{c.id}</th>
+                          <td>
+                            <div className="form-group">
+                              <Field
+                                type="text"
+                                className={`form-control ${
+                                  errors.name && touched.name
+                                    ? "is-invalid"
+                                    : ""
+                                }`}
+                                placeholder="Name"
+                                name="name"
+                                aria-label="Name"
+                                aria-describedby="basic-addon2"
+                              />
+                              <ErrorMessage
+                                name="name"
+                                component="div"
+                                className="invalid-feedback"
+                              />
+                            </div>
+                          </td>
+                          <td>
+                            <div className="form-group">
+                              <Field
+                                type="text"
+                                className={`form-control ${
+                                  errors.description && touched.description
+                                    ? "is-invalid"
+                                    : ""
+                                }`}
+                                placeholder="Description"
+                                name="description"
+                                aria-label="Description"
+                                aria-describedby="basic-addon2"
+                              />
+                              <ErrorMessage
+                                name="description"
+                                component="div"
+                                className="invalid-feedback"
+                              />
+                            </div>
+                          </td>
+                          <td>
+                            <div className="form-group">
+                              <Field
+                                type="text"
+                                className={`form-control ${
+                                  errors.image && touched.image
+                                    ? "is-invalid"
+                                    : ""
+                                }`}
+                                placeholder="Image url"
+                                name="image"
+                                aria-label="Image url"
+                                aria-describedby="basic-addon2"
+                              />
+                              <ErrorMessage
+                                name="image"
+                                component="div"
+                                className="invalid-feedback"
+                              />
+                            </div>
+                          </td>
+                          <td>
+                            <button
+                              type="submit"
+                              className="btn btn-warning btn-sm"
+                            >
+                              <i className="bi bi-cloud-download"></i>
+                            </button>
+                          </td>
+                        </tr>
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
